c4b41942f65ebe1bb002e1a54e140adc2e6f9182
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2025-01-20 02:10:34 +0100

    Making animations more smooth when handled by arrows.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
             setIsRotating={setIsRotating}
             setCurrentStage={setCurrentStage}
           />
-          <Plane isRotating={isRotating} planeScale={planeScale} planePosition={planePosition} rotation={[0, 20, 0]} />
+          <Plane isRotating={isRotating} scale={planeScale} position={planePosition} rotation={[0, 20, 0]} />
         </Suspense>
       </Canvas>
     </section>
